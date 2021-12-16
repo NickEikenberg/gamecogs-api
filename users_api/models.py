4feb71628e5f550b62d109d5788a43064d42bb96
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-15 19:58:58 -0500

    initial commit

---
+++ users_api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class UserAccount(models.Model):
+  email = models.CharField(max_length=75, unique=True)
+  password = models.CharField(max_length=1000)
---
+++ users_api/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserAccountSerializer
+from .models import UserAccount
+
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import JsonResponse
+import json
+
+# Create your views here.
+
+class UserAccountList(generics.ListCreateAPIView):
+  queryset = UserAccount.objects.all().order_by('id')
+  serializer_class = UserAccountSerializer
+
+class UserAccountDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = UserAccount.objects.all().order_by('id')
+  serializer_class = UserAccountSerializer
+
+## USER AUTH
+def check_login(request):
+  # If a GET request is made, return an empty object
+  if request.method=='GET':
+    return JsonResponse({})
+
+  if request.method=='PUT':
+    # Make the request JSON
+    jsonRequest = json.loads(request.body)
+    email = jsonRequest['email']
+    password = jsonRequest['password']
+
+    # See if email exists in DB
+    if UserAccount.objects.get(email=email):
+      user = UserAccount.objects.get(email=email)
+
+      if check_password(password, user.password):
+        return JsonResponse({'id': user.id, 'email': user.email})
+      else:
+        return JsonResponse({})
+    else:
+      return JsonResponse({})
