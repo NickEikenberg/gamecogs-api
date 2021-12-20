0fa7d578a19963e9a582a6269ca2a919c2e2f90c
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-19 21:19:44 -0500

    Added user lists to models

--- users_api/models.py
+++ users_api/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class UserAccount(models.Model):
   email = models.CharField(max_length=75, unique=True)
   password = models.CharField(max_length=1000)
+  collection = models.CharField(max_length=10000, default='test')
+  forsale = models.CharField(max_length=10000, default='test')
+  wantlist = models.CharField(max_length=10000, default='test')
